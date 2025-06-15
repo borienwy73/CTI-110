07c5027c8938768c1608366d0c6c83d4ab94558d
borienwy73 <cruzb1497@student.faytechcc.edu>
2025-06-15 15:27:47 -0400

    Add files via upload
    First submission Input and output.

---
+++ P1LAB1_CruzBrandon.py
@@ -0,0 +1,10 @@
+#Brandon R. Cruz
+#15 June 2025
+#P1LAB1
+#Initial input from user and display output
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print ("Hello", first_name, last_name +"!" , "Welcome to CTI-110")
