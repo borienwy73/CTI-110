06ffebba030c1bacc938d7d252ba8b4b055db5b4
borienwy73 <cruzb1497@student.faytechcc.edu>
2025-06-16 15:09:50 -0400

    Add files via upload
    Mathematical Calculation

---
+++ P1HW1_CruzBrandon.py
@@ -0,0 +1,37 @@
+# Brandon R Cruz
+# 15 June 2025
+#P1HW1
+#Initial calculation of exponents and ddition and substraction
+
+
+
+
+
+print("-----Calculating Exponents-----")
+print()
+print()
+
+num1 = int(input("Enter an integer as the base value: "))
+num2 = int(input("Enter an integer as the exponent: "))
+
+result = num1 ** num2
+
+print()
+print()
+print(num1, "raised to the power of", num2, "is", result, "!!")
+
+print()
+print()
+print("-----Addition and Subtraction-----")
+print()
+print()
+
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+result = num1 + num2 - num3
+
+print()
+print()
+print(num1, "+", num2, "-", num3, "is equal to", result)
